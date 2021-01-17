1d0298b8fdf794b442a298182193af2e6ccf193c
vocarlo <carlo96prod@gmail.com>
2021-01-17 15:15:29 -0400

    multiple responsive fixes & footer

---
+++ Landing/src/index.js
@@ -0,0 +1,8 @@
+// const arrowButton = window.document.getElementById('arrow-button');
+
+// const desplace = (e) => {
+//     console.log(e);
+//     console.log(screen.height);
+// }
+
+// arrowButton.onclick = desplace;
